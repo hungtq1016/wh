c3164a1190fbd12f4a2e9ac6763f5adfed002289
hungtq1016 <hungtq1016@gmail.com>
2024-08-04 16:23:16 +0700

    update: remove unneccessary page

--- src/app/(dashboard)/home/layout.tsx
+++ src/app/(dashboard)/home/layout.tsx
@@ -2,7 +2,7 @@
 import { AsideContext } from '@/libs/contexts/AsideContext'
 import { useContext, useEffect, useMemo } from 'react'
 import { usePathname } from 'next/navigation'
-import { CompareArrowsOutlined, ContactEmergencyOutlined, FilterFramesOutlined, InfoOutlined, PanoramaOutlined, RecentActorsOutlined, ShoppingCartCheckout, SlideshowOutlined, ViewCarouselOutlined, ViewColumnOutlined, VrpanoOutlined } from '@mui/icons-material'
+import { CompareArrowsOutlined, ContactEmergencyOutlined, InfoOutlined, PanoramaOutlined, RecentActorsOutlined, ShoppingCartCheckout, SlideshowOutlined, ViewCarouselOutlined, ViewColumnOutlined, VrpanoOutlined } from '@mui/icons-material'
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
     const { asideDispatch } = useContext(AsideContext)
     const pathname = usePathname()
@@ -20,13 +20,6 @@ export default function ProductsLayout({ children }: { children: React.ReactNode
             href: '/home/cover',
             icon: ViewColumnOutlined,
             value: 'view',
-            current: false
-        },
-        {
-            name: 'About Section',
-            href: '/home/about',
-            value: 'view',
-            icon: ViewCarouselOutlined,
             current: false
         },
         {
@@ -58,13 +51,6 @@ export default function ProductsLayout({ children }: { children: React.ReactNode
             current: false
         },
         {
-            name: 'Frame Section',
-            href: '/home/frame',
-            value: 'view',
-            icon: FilterFramesOutlined,
-            current: false
-        },
-        {
             name: 'Author Section',
             href: '/home/author',
             value: 'view',
@@ -78,14 +64,6 @@ export default function ProductsLayout({ children }: { children: React.ReactNode
             icon: InfoOutlined,
             current: false
         },
-        {
-            name: 'Shop Section',
-            href: '/home/shop',
-            value: 'view',
-            icon: ShoppingCartCheckout,
-            current: false
-        },
-        
     ], []); 
 
     useEffect(() => {
